1d0378c1d138ab4bff2650885c13c37f524fd398
Sam Sarette <samuelsarette@linux.com>
2012-11-08 18:04:17 -0500

    fixed local file app launchers

--- launch-webapp-noserver.sh
+++ launch-webapp-noserver.sh
@@ -5,10 +5,10 @@ if [ "$(uname)" == "Darwin" ] ; then
         osascript -e 'tell app "System Events" to display dialog "Advanced Web App - Could not find Google Chrome!"'
         exit 1
     fi
-    open -na "/Applications/Google Chrome.app" --args --app=./index.html
+    open -na "/Applications/Google Chrome.app" --args --app=file://`pwd`/index.html
 elif [ "$(uname)" == "Linux" ] ; then
     command -v google-chrome >/dev/null 2>&1 || { notify-send "Advanced Web App" "Could not find Google Chrome\!" || echo "Advanced Web App - Could not find Google Chrome!"; exit 1; }
-    google-chrome --app=./index.html
+    google-chrome --app=file://`pwd`/index.html
 else
     echo "Unknown operating system!"
     exit 1
