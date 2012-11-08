844b2f981279015dd6356a3ae180b72bcda6558c
Sam Sarette <samuelsarette@linux.com>
2012-11-08 16:44:45 -0500

    Added Win7 support to launch bat

--- launch-webapp.bat
+++ launch-webapp.bat
@@ -1,10 +1,15 @@
 @echo off
 for /f "tokens=3 delims=\" %%i in ("%USERPROFILE%") do (set user=%%i) 2>&1
-set CHROME_LOCAL="C:\Documents and Settings\%user%\Local Settings\Application Data\Google\Chrome\Application\chrome.exe"
+set CHROME_XP="C:\Documents and Settings\%user%\Local Settings\Application Data\Google\Chrome\Application\chrome.exe"
+set CHROME_7="C:\Users\%user%\AppData\Local\Google\Chrome\Application\chrome.exe"
 
-IF EXIST %CHROME_LOCAL% (
-    %CHROME_LOCAL% --app=http://localhost/advanced-webapp-demo/index.html
+IF EXIST %CHROME_XP% (
+    %CHROME_XP% --app=http://localhost/advanced-webapp-demo/index.html
 ) ELSE (
-    echo Could not find Google Chrome.
-    pause
+    IF EXIST %CHROME_7% (
+        %CHROME_7% --app=http://localhost/advanced-webapp-demo/index.html
+    ) ELSE (
+        echo Could not find Google Chrome.
+        pause
+    )
 )
