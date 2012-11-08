d3fa3f5ddee1af3705dfc7ed84f26c84c92fbbba
Sam Sarette <samuelsarette@linux.com>
2012-11-08 16:23:41 -0500

    Added support for OSX in other launch script

--- launch-webapp-noserver.sh
+++ launch-webapp-noserver.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
-command -v google-chrome >/dev/null 2>&1 || { notify-send "Advanced Web App" "Couldn't find Google Chrome\!"; exit 1; }
-
-google-chrome --app=./index.html
+if [ "$(uname)" == "Darwin" ] ; then
+    if [ ! -d "/Applications/Google Chrome.app" ] ; then
+        osascript -e 'tell app "System Events" to display dialog "Advanced Web App - Could not find Google Chrome!"'
+        exit 1
+    fi
+    open -a "/Applications/Google Chrome.app" --args --app=./index.html
+elif [ "$(uname)" == "Linux" ] ; then
+    command -v google-chrome >/dev/null 2>&1 || { notify-send "Advanced Web App" "Could not find Google Chrome\!" || echo "Advanced Web App - Could not find Google Chrome!"; exit 1; }
+    google-chrome --app=./index.html
+else
+    echo "Unknown operating system!"
+    exit 1
+fi
