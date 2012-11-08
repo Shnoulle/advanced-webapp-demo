c4f2fd2672245bc4956bc522f918b1557e489434
Sam Sarette <samuelsarette@linux.com>
2012-11-08 16:48:08 -0500

    Updated OSX parameter

--- launch-webapp.sh
+++ launch-webapp.sh
@@ -5,7 +5,7 @@ if [ "$(uname)" == "Darwin" ] ; then
         osascript -e 'tell app "System Events" to display dialog "Advanced Web App - Could not find Google Chrome!"'
         exit 1
     fi
-    open -a "/Applications/Google Chrome.app" --args --app=http://localhost/advanced-webapp-demo/index.html
+    open -na "/Applications/Google Chrome.app" --args --app=http://localhost/advanced-webapp-demo/index.html
 elif [ "$(uname)" == "Linux" ] ; then
     command -v google-chrome >/dev/null 2>&1 || { notify-send "Advanced Web App" "Could not find Google Chrome\!" || echo "Advanced Web App - Could not find Google Chrome!"; exit 1; }
     google-chrome --app=http://localhost/advanced-webapp-demo/index.html
