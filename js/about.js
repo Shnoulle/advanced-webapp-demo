14d0dc4915f8a5a7a2aff553b9f19ab8713f4d3b
Sam Sarette <samuelsarette@linux.com>
2012-11-08 20:52:02 -0500

    Lot's of small screen fixes

--- js/about.js
+++ js/about.js
@@ -6,25 +6,19 @@ var aboutModalHTML =
 "       <h3>About</h3>" +
 "   </div>" +
 "   <div class='modal-body'>" +
-"       <div id='modal-fix1' style='width: 100%'>" +
-"           <div class='row'>" +
-"               <div class='span1'>" +
-"                   <img src='img/icon.png' style='text-align: right; width: 128px;' />" +
+"       <div class='row-fluid'>" +
+"               <div class='span4'>" +
+"                   <img id='about-icon' src='img/icon.png' style='text-align: right; width: 128px; padding-top: 30px;' />" +
 "               </div>" +
-"               <div id='modal-fix2' class='span4' style='margin-left: 20px;'>" +
-"                   <div class='row'>" +
-"                       <div class='span4'>" +
-"                           <h2>Advanced Web App</h2>" +
-"                           <p style='padding-left: 20px;'>A simple demonstration for an HTML5 application.</p>" +
-"                           <hr />" +
-"                           <p>Author: Samuel Sarette</p>" +
-"                           <p>Email: samuelsarette@linux.com</p>" +
-"                       </div>" +
-"                   </div>" +
+"               <div id='modal-fix2' class='span7' style='margin-left: 20px;'>" +
+"                   <h2>Advanced Web App</h2>" +
+"                   <p style='padding-left: 20px;'>A simple demonstration for an HTML5 application.</p>" +
+"                   <hr />" +
+"                   <p>Author: Samuel Sarette</p>" +
+"                   <p>Email: samuelsarette@linux.com</p>" +
 "               </div>" +
 "           </div>" +
 "       </div>" +
-"   </div>" +
 "   <div class='modal-footer'>" +
 "       <a href='#' data-dismiss='modal' class='btn' >Close</a>" +
 "   </div>" +
