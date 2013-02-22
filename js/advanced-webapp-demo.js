b52b0a347be2e15a323a41151e7f9073286cd0d7
Sam Sarette <samuelsarette@linux.com>
2013-02-21 19:57:24 -0500

    Added Results history.

--- js/advanced-webapp-demo.js
+++ js/advanced-webapp-demo.js
@@ -11,19 +11,59 @@ if (localStorage)
 }
 
 var filesystemapisupport = false;
-window.requestFileSystem = window.requestFileSystem || window.webkitRequestFileSystem;
-if (window.requestFileSystem)
+var fs = null;
+var fsCallbacks = [];
+function setupFileSystemAPI(forCallback)
 {
-    window.requestFileSystem(window.TEMPORARY, 5*1024*1024, function(fs){
-        console.log('Filesystem API support!');
-        filesystemapisupport = true;
-    }, function(){
-        console.log('No Filesystem API support');
-    });
-}
-else
-{
-        console.log('No Filesystem API support');
+    if (forCallback != null || typeof(forCallback) != "undefined")
+    {
+        if (fs != null)
+        {
+            forCallback();
+            return;
+        }
+        else
+        {
+            fsCallbacks.push(forCallback);
+        }
+    }
+
+    window.requestFileSystem = window.requestFileSystem || window.webkitRequestFileSystem;
+    if (window.requestFileSystem)
+    {
+        var newQuotaInBytes = 25*1024*1024;
+        window.requestFileSystem(window.PERSISTENT, 
+                                newQuotaInBytes,
+                                function(filesystem)
+                                {
+                                    fs = filesystem;
+                                    filesystemapisupport = true;
+                                    webkitStorageInfo.requestQuota( webkitStorageInfo.PERSISTENT,
+                                        newQuotaInBytes,
+                                        function(){
+                                            if (fsCallbacks != null)
+                                            {
+                                                var runningCallbacks = fsCallbacks.slice();
+                                                fsCallbacks = null;
+                                                for (var i = 0; i < runningCallbacks.length; i++)
+                                                {
+                                                    runningCallbacks[i]();
+                                                }
+                                            }
+                                        },
+                                        function(){
+                                            alert('Accept the request for filesystem storage permission.');
+                                            filesystemapisupport = false;
+                                        });
+                                }, function(){
+                                    console.log('No Filesystem API support');
+                                });
+
+    }
+    else
+    {
+            console.log('No Filesystem API support');
+    }
 }
 
 
@@ -41,6 +81,8 @@ include("js/about.js", function()
 $(window).bind("unload", function() { }); // need this to fix firefox back() issue
 $(document).ready(function()
 {
+    setupFileSystemAPI();
+    
     /* disable context menu */
     $(document).bind("contextmenu",function(e){
         return false;
@@ -250,4 +292,92 @@ function notify(title, message)
             }
         }
     }
+}
+
+/* http://www.bennadel.com/blog/1504-Ask-Ben-Parsing-CSV-Strings-With-Javascript-Exec-Regular-Expression-Command.htm */
+// This will parse a delimited string into an array of
+// arrays. The default delimiter is the comma, but this
+// can be overriden in the second argument.
+function CSVToArray( strData, strDelimiter )
+{
+    // Check to see if the delimiter is defined. If not,
+    // then default to comma.
+    strDelimiter = (strDelimiter || ",");
+    if (!strData || 0 === strData.length || /^\s*$/.test(strData) || strData.indexOf(strDelimiter) === -1) return null; // return empty array if impossible to find any info
+    
+    // Create a regular expression to parse the CSV values.
+    var objPattern = new RegExp(
+        (
+            // Delimiters.
+            "(\\" + strDelimiter + "|\\r?\\n|\\r|^)" +
+            
+            // Quoted fields.
+            "(?:\"([^\"]*(?:\"\"[^\"]*)*)\"|" +
+            
+            // Standard fields.
+            "([^\"\\" + strDelimiter + "\\r\\n]*))"
+            ),
+            "gi"
+            );
+            
+            
+            // Create an array to hold our data. Give the array
+            // a default empty first row.
+            var arrData = [[]];
+            
+            // Create an array to hold our individual pattern
+            // matching groups.
+            var arrMatches = null;
+            
+            
+            // Keep looping over the regular expression matches
+            // until we can no longer find a match.
+            while (arrMatches = objPattern.exec( strData )){
+                
+                // Get the delimiter that was found.
+                var strMatchedDelimiter = arrMatches[ 1 ];
+                
+                // Check to see if the given delimiter has a length
+                // (is not the start of string) and if it matches
+                // field delimiter. If id does not, then we know
+                // that this delimiter is a row delimiter.
+                if (
+                    strMatchedDelimiter.length &&
+                    (strMatchedDelimiter != strDelimiter)
+                ){
+                    
+                    // Since we have reached a new row of data,
+                    // add an empty row to our data array.
+                    arrData.push( [] );
+                    
+                }
+                
+                
+                // Now that we have our delimiter out of the way,
+                // let's check to see which kind of value we
+                // captured (quoted or unquoted).
+                if (arrMatches[ 2 ]){
+                    
+                    // We found a quoted value. When we capture
+                    // this value, unescape any double quotes.
+                    var strMatchedValue = arrMatches[ 2 ].replace(
+                        new RegExp( "\"\"", "g" ),
+         "\""
+        );
+        
+                } else {
+                    
+                    // We found a non-quoted value.
+                    var strMatchedValue = arrMatches[ 3 ];
+                    
+                }
+                
+                
+                // Now that we have our value string, let's add
+                // it to the data array.
+                arrData[ arrData.length - 1 ].push( parseInt(strMatchedValue) );
+            }
+            
+            // Return the parsed data.
+            return( arrData );
 }
--- js/resultsdata.js
+++ js/resultsdata.js
@@ -1,14 +1,43 @@
 /* Test Results specific functions */
 
-function logNewResults(results)
+function createRandomResults()
 {
-    return results;
+    return [[Math.floor(Math.random()*7),
+            Math.floor(Math.random()*13),
+            Math.floor(Math.random()*18),
+            Math.floor(Math.random()*30),
+            Math.floor(Math.random()*50),
+            Math.floor(Math.random()*80),
+            Math.floor(Math.random()*90),
+            Math.floor(Math.random()*70),
+            Math.floor(Math.random()*60),
+            Math.floor(Math.random()*30)
+            ]];
 }
 
-function openResults(filename)
+function logNewResults(results)
 {
-    var results = [[3,7,9,10,40,60,38,24,15]];
-    
+    /* Temporary */
+    createResultsFolder(function(){
+        currentFile = null;
+        createResultFile(function(){
+            var csv = "";
+            for(var i = 0; i < results.length; i++) 
+            {
+                for(var j = 0; j < results[i].length; j++) 
+                {
+                    csv += results[i][j];
+                    if ((j + 1) != results[i].length) csv += ",";
+                }
+                if ((i + 1) != results.length) csv += "\n";
+            }
+            writeToFile(currentFile, csv);
+        });
+    });
+}
+
+function openResults(results)
+{
     return $.jqplot('chart1', results, {
         series:[{showMarker:false}],
         axes:{
@@ -22,71 +51,143 @@ function openResults(filename)
     });
 }
 
+function exportCurrentResults()
+{
+    exportFileContents(currentFile.name, currentFileContents);
+}
+
+
 /* File operations */
 
-function createResultsFolder()
+function createResultsFolder(callback)
 {
+    setupFileSystemAPI();
     fs.root.getDirectory('ResultsData', {create: true}, function(dirEntry) {
         console.log('You have just created the ' + dirEntry.name + ' directory.');
+        callback();
     }, errorHandler);
 }
 
-function getResultsFiles()
+var resultFiles = null;
+
+function getResultsFiles(resultsCallback)
 {
-    fs.root.getDirectory('ResultsData', {}, function(dirEntry){
-        var dirReader = dirEntry.createReader();
-        dirReader.readEntries(function(entries) {
-        for(var i = 0; i < entries.length; i++) {
-            var entry = entries[i];
-            if (entry.isDirectory){
-                console.log('Directory: ' + entry.fullPath);
+    setupFileSystemAPI(function(){
+        fs.root.getDirectory('ResultsData', {}, function(dirEntry){
+            var dirReader = dirEntry.createReader();
+            var results = [];
+            dirReader.readEntries(function(entries) {
+            for(var i = 0; i < entries.length; i++) {
+                var entry = entries[i];
+                if (entry.isDirectory){
+                    //don't care
+                }
+                else if (entry.isFile){
+                    results.push(entry);
+                }
             }
-            else if (entry.isFile){
-                console.log('File: ' + entry.fullPath);
-            }
-        }
+            resultFiles = results;
+            if (typeof(resultsCallback) != "undefined" && resultsCallback != null) resultsCallback();
+            }, errorHandler);
         }, errorHandler);
-    }, errorHandler);
+    });
 }
 
 var currentFile = null;
 var currentFileContents = null;
 
-function createResultFile()
+function createResultFile(callback)
 {
+    setupFileSystemAPI();
     var milliseconds = (new Date).getTime();
-    fs.root.getFile('results-' + milliseconds + '.csv', {create: true, exclusive: true}, function(fileEntry) {
+    fs.root.getFile('ResultsData/results-' + milliseconds + '.csv', {create: true, exclusive: true}, function(fileEntry) {
         console.log('A file ' + fileEntry.name + ' was created successfully.');
         currentFile = fileEntry;
+        callback();
     }, errorHandler);
 }
 
-function readFile(filename)
+function getFile(filename)
 {
+    setupFileSystemAPI();
     fs.root.getFile(filename, {create: false, exclusive: true}, function(fileEntry) {
         console.log('A file ' + fileEntry.name + ' was created successfully.');
         currentFile = fileEntry;
     }, errorHandler);
 }
 
-function writeToFile(fileEntry, text)
+function writeToFile(fileEntry, text, callback)
 {
+    setupFileSystemAPI();
     fileEntry.createWriter(function(fileWriter) {
         fileWriter.seek(fileWriter.length);
-        window.BlobBuilder = window.BlobBuilder || window.WebKitBlobBuilder;
-        var bb = new BlobBuilder();
-        bb.append(text);
-        fileWriter.write(bb.getBlob('text/plain'));
+        var blob = new Blob([text], {type: 'text/plain'});
+        fileWriter.write(blob);
+        readFile(currentFile);
+        if (!!callback) callback();
     }, errorHandler);
 }
 
-function readFile(fileEntry)
+function exportFileContents(name, contents)
 {
-    fileEntry.file(function(file) {
-        var reader = new FileReader();
-        reader.onloadend = function(e) {
-            currentFileContents = this.result;
-        };
-        reader.readAsText(file);
-    }, errorHandler);
+    setupFileSystemAPI();
+    var blob = new Blob([contents], {type: "text/plain;charset=utf-8"});
+    saveAs(blob, name);
+}
+
+var readFileCallback;
+function readFile(fileEntry, callback)
+{
+    readFileCallback = callback;
+    currentFile = fileEntry;
+    setupFileSystemAPI(function(){
+        currentFile.file(function(file) {
+            var reader = new FileReader();
+            reader.onloadend = function(e) {
+                currentFileContents = this.result;
+            };
+            reader.readAsText(file);
+            if (typeof(callback) != "undefined" && callback != null) readFileCallback();
+        }, errorHandler);
+    });
+}
+
+function errorHandler(err)
+{
+    switch (err.code)
+    {
+        case FileError.NOT_FOUND_ERR:
+            console.log("Error: File Not Found");
+            break;
+        case FileError.SECURITY_ERR:
+            console.log("Error: Security Error");
+            break;
+        case FileError.ABORT_ERR:
+            console.log("Error: Aborted");
+            break;
+        case FileError.NOT_READABLE_ERR:
+            console.log("Error: Not Readable");
+            break;
+        case FileError.ENCODING_ERR:
+            console.log("Error: Encoding Issues");
+            break;
+        case FileError.NO_MODIFICATION_ALLOWED_ERR:
+            console.log("Error: Not Allowed to Modify");
+            break;
+        case FileError.INVALID_STATE_ERR:
+            console.log("Error: Invalid State");
+            break;
+        case FileError.SYNTAX_ERR:
+            console.log("Error: Syntax");
+            break;
+        case FileError.INVALID_MODIFICATION_ERR:
+            console.log("Error: Invalid Modification");
+            break;
+        case FileError.QUOTA_EXCEEDED_ERR:
+            console.log("Error: Quota Exceeded");
+            break;
+        case FileError.TYPE_MISMATCH_ERR:
+            console.log("Error: Type Mismatch");
+            break;
+    }
 }
